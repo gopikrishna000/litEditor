d1629decb0b34b628b5cec96970242e50bc06e18
Aman Mark <amanmarkkujur07@gmail.com>
2021-05-15 22:37:38 +0530

    Unchecked

--- src/ui/md_preview_frame.py
+++ src/ui/md_preview_frame.py
@@ -1,8 +1,17 @@
 from tkinter import Frame
+from tkhtmlview import HTMLLabel
 
 from src.utils import Observable
 
 
 def get_md_preview_frame(master, html_string: Observable):
-    # todo: observe changes from html_string
-    return Frame(master)
+
+    md_prev_frame = HTMLLabel(master, background='white')
+    val = md_prev_frame.get()
+
+    def on_html_change():
+        if val or val == '':
+            html_string.dispatch(val)
+
+    html_string.observe(on_html_change)
+    return md_prev_frame
