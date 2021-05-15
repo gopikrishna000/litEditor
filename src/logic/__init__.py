3ce0f592893b5bdb54a4fc58fe0645db5f803c82
Pushpavel <jpushpavel@gmail.com>
2021-05-15 21:07:02 +0530

    simplified side_nav.py

--- src/logic/__init__.py
+++ src/logic/__init__.py
@@ -1,3 +1,3 @@
 from src.logic.file_logic import use_file_logic
 from src.logic.html_logic import use_html_logic
-from src.logic.tab_page_ui_logic import use_tab_page_ui_logic
+from src.logic.tab_page_ui_logic import use_tab_page_ui_logic, tabs
--- src/ui/side_nav.py
+++ src/ui/side_nav.py
@@ -1,53 +1,31 @@
-#from src.utils import Lifecycle
-from tkinter import*
-from tkinter import ttk
-from tkinter import scrolledtext
-from tkinter import Menu
-from tkinter.constants import NO
-from PIL import Image,ImageTk
-  
-    
+from tkinter import *
 
-def get_side_nav(master,selected_tab,tabs):
-
-    buttondict = dict.fromkeys(tabs,None)
-    
-    tab_frame=Frame(master,bg='#92C569')  
-    i=0
-    for name in tabs:
-        buttondict[name]=Button(tab_frame,height=2,width=10,text=name,bg='#92C569')
-        buttondict[name].grid(row=i,column=0,sticky=W) 
-        i=i+1
-
-    funcdict ={'new.png':create_file,'folder.jpg':select_folder,'edit.png':edit_markdown,'lit.jpg':lit,'preview.png':preview,'setting.jpg':setting,'exit.png':exit}
-    
-    
-    for name in buttondict:
-            buttondict[name].configure(command=funcdict[name])
-
-    return tab_frame    
+from src.logic import tabs
 
 
+def get_side_nav(master, selected_tab):
+    tab_frame = Frame(master, bg='#92C569')
 
-def select_folder():
-    pass
-def create_file():
-    pass
-def edit_markdown():
-    pass
-def lit():
-    pass
-def preview():
-    pass
-def setting():
-    pass
-def exit():
-    pass
+    buttons = dict()
+    last_selected_tab = selected_tab.value
 
-'''
-tk=Tk()
-tabs=['new.png','folder.jpg','edit.png','lit.jpg','preview.png','setting.jpg','exit.png']
-tab_frame=get_side_nav(tk,tabs)
-tab_frame.pack(fill='y',side='left',ipadx=0,ipady=0)
-tk.mainloop()
-'''
+    for index, key in enumerate(tabs):
+        button = Button(tab_frame, height=2, width=10, text=key, bg='#92C569')
+        button.grid(row=index, column=0, sticky=W)
+        button.configure(command=lambda: selected_tab.dispatch(key))
+        buttons.setdefault(key, button)
+
+    def on_tab_selected(_key):
+        nonlocal last_selected_tab
+
+        _last_button = buttons.get(last_selected_tab)
+        # remove selected look from this button
+
+        _button = buttons.get(key)
+        # update selected look for this button
+
+        last_selected_tab = key
+
+    selected_tab.observe(on_tab_selected)
+
+    return tab_frame
