6234fa49206332f6d5435798337ae3b6a0e35774
Aman Mark <amanmarkkujur07@gmail.com>
2021-05-15 23:03:44 +0530

    Edited Preview_Frame

--- src/ui/md_preview_frame.py
+++ src/ui/md_preview_frame.py
@@ -7,7 +7,6 @@ from src.utils import Observable
 def get_md_preview_frame(master, html_string: Observable):
 
     md_prev_frame = HTMLLabel(master, background='white')
-    val = md_prev_frame.get()
 
     def on_html_change(html_text):
         md_prev_frame.set_html(html_text)
