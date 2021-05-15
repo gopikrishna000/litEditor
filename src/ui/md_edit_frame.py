91b1574329d798aca2d3201c8d5364bd5a319ff8
Aman Mark <amanmarkkujur07@gmail.com>
2021-05-15 22:06:42 +0530

    Updated Edit_Frame

--- src/ui/md_edit_frame.py
+++ src/ui/md_edit_frame.py
@@ -1,9 +1,16 @@
-from tkinter import Text
+from tkinter import Text,font
 
 from src.utils import Observable
 
 
 def get_md_edit_frame(master, markdown_string: Observable):
-    # todo: observe changes from markdown_string
-    # todo: update markdown_string on changes
-    return Text()
+
+    md_edit_frame = Text(master, font=font.Font(family='arial', size=14))
+
+    def on_md_change():
+        val = md_edit_frame.get()
+        if val or val == '':
+            markdown_string.dispatch(val)
+
+    markdown_string.observe(on_md_change)
+    return md_edit_frame
