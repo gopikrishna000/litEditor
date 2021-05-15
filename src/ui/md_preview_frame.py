e233cb5b2ddbe539f87f4decfff796225aadc7be
Aman Mark <amanmarkkujur07@gmail.com>
2021-05-15 23:03:19 +0530

    Edited Preview_Frame

--- src/ui/md_preview_frame.py
+++ src/ui/md_preview_frame.py
@@ -9,9 +9,9 @@ def get_md_preview_frame(master, html_string: Observable):
     md_prev_frame = HTMLLabel(master, background='white')
     val = md_prev_frame.get()
 
-    def on_html_change():
-        if val or val == '':
-            html_string.dispatch(val)
+    def on_html_change(html_text):
+        md_prev_frame.set_html(html_text)
+        html_string.dispatch(html_text)
 
     html_string.observe(on_html_change)
     return md_prev_frame
