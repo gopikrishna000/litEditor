2a858769e2ab43da602f49cf0f166bfa182dddf8
Aman Mark <amanmarkkujur07@gmail.com>
2021-05-16 11:37:53 +0530

    Added the Show Recent Files feature

--- src/ui/file_frame.py
+++ src/ui/file_frame.py
@@ -1,4 +1,4 @@
-from tkinter import Frame, Label, Button, filedialog, StringVar
+from tkinter import Frame, Label, Button, filedialog, StringVar, Listbox
 
 valid_file_types = (
     ("Github Markdown File", "*.(markdown|mdown|mkdn|md|mkd)"),
@@ -30,14 +30,24 @@ def get_file_frame(master, file_path_var: StringVar):
     create_btn.pack(expand=True, fill='x', side='left', padx=4, pady=4)
     control_frame.pack()
 
+    test_frame = Frame(parent)
+    test_label = Label(test_frame, text='Recently Opened Files', font=('arial', 12))
+    recent_drop = Listbox(test_frame, height=5, width=50, font=('arial', 10))
+
+    test_label.pack(fill='x', padx=4, pady=4)
+    recent_drop.pack(expand=True, side='right', padx=4, pady=4)
+    test_frame.pack()
+
     def on_browse_btn():
         file_path = filedialog.askopenfilename(filetypes=valid_file_types)
         file_path_var.set(file_path)
+        recent_drop.insert(0, file_path_var.get())
 
     def on_create_btn():
         file_path = filedialog.asksaveasfilename(filetypes=valid_file_types)
         #   todo: verify file extension
         file_path_var.set(file_path)
+        recent_drop.insert(0, file_path_var.get())
 
     browse_btn.configure(command=on_browse_btn)
     create_btn.configure(command=on_create_btn)
