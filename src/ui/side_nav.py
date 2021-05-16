57fbf34bab545f6928c762747efac2a9942cd519
gopikrishna <gopikrishna01012001@gmail.com>
2021-05-16 08:23:24 +0530

    icon sunken side_nav

--- src/ui/side_nav.py
+++ src/ui/side_nav.py
@@ -10,7 +10,7 @@ def get_side_nav(master, selected_tab: StringVar):
     last_selected_tab = selected_tab.get()
 
     for index, key in list(enumerate(tabs)):
-        button = Button(tab_frame, height=2, width=10, text=key, bg='#92C569')
+        button = Button(tab_frame, height=2, width=10, text=key, bg='#92C569',relief=FLAT)
         button.grid(row=index, column=0, sticky=W)
         buttons.setdefault(key, button)
         button.configure(command=lambda k=key: selected_tab.set(k))
@@ -21,9 +21,12 @@ def get_side_nav(master, selected_tab: StringVar):
 
         _last_button = buttons.get(last_selected_tab)
         # remove selected look from this button
-
-        _button = buttons.get(key)
+        _last_button.configure(relief=FLAT)
+        _button = buttons.get(_key)
         # update selected look for this button
+        _button.configure(relief=SUNKEN)
+        print(_key)
+        last_selected_tab = _key
 
         last_selected_tab = key
 
