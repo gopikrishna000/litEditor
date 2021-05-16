1d9c179f4be06e74a352b2bcc8db782f43a7691a
Pushpavel <jpushpavel@gmail.com>
2021-05-16 10:21:00 +0530

    dpi aware windows

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # entry point to the program
+import ctypes
 from tkinter import Tk
 
 from src import lit_editor
+
+ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
 window = Tk()
 
 window.title('litEditor')
 window.minsize('720', '480')
+# window.tk.call('tk', 'scaling', 2.0)
 
 editor = lit_editor(window)
 editor.pack(expand=True, fill='both')
